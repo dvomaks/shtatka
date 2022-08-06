442d847ea7137bc701bcb958147b10840eeb0f31
DSST <dsst@Makss-MBP.lan>
2022-08-06 17:49:09 +0300

    Initial

---
+++ components/VzvodItem.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "VzvodItem"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/inspire.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              Item
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-row>
+                <v-col>
+
+                </v-col>
+                <v-col>
+
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage'
+}
+</script>
